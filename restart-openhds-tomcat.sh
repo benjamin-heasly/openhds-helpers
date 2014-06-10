c1b6f59e2af8c428feffffb8564c4fa2d06b90d9
Ben Heasly <benjamin.heasly@gmail.com>
2014-06-10 13:20:33 -0400

    update OpenHDS config files to include with deployments

--- restart-openhds-tomcat.sh
+++ restart-openhds-tomcat.sh
@@ -2,7 +2,7 @@
 source openhds-tomcat-env.sh
 ./stop-openhds-tomcat.sh
 echo "starting OpenHDS Tomcat"
-sudo $TOMCAT_FOLDER/bin/catalina.sh start
+sudo $TOMCAT_FOLDER/bin/catalina.sh jpda start
 
 # stream the Tomcat output
 tail -f $TOMCAT_FOLDER/logs/catalina.out
